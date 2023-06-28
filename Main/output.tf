f0f0b174e914ac2212e4d93ea44ece5d90e6db87
hocine699 <127848706+hocine699@users.noreply.github.com>
2023-06-28 14:43:46 +0200

    Update output.tf

--- Main/output.tf
+++ Main/output.tf
@@ -1,9 +1,9 @@
 output "Cle_publique" {
-  value     = module.EnfantModule.pub
+  value     = module.ModuleEnfant.pub
   sensitive = true
 }
 
 output "Cle_privee" {
-    value   = module.EnfantModule.private
+    value   = module.ModuleEnfant.private
     sensitive = true
 }
